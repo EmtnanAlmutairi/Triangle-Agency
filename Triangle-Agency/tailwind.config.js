5e37bfeb33212f9d1503f5a704a97c85c04fcfd4
امتنان المطيري <117547275+EmtnanAlmutairi@users.noreply.github.com>
2024-06-13 08:54:33 +0300

    Update tailwind.config.js

--- Triangle-Agency/tailwind.config.js
+++ Triangle-Agency/tailwind.config.js
@@ -4,7 +4,6 @@ import {nextui} from '@nextui-org/react'
 module.exports = {
   content: [
     './layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
