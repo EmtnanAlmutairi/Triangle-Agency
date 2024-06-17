acd45349f7efe56f80e9f674d421202d1c0424d7
امتنان المطيري <imeland925@gmail.com>
2024-06-18 02:29:09 +0300

    fix

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import { Button } from "@nextui-org/react";
 
 const Hero: React.FC = () => {
     return (
         <div className="flex flex-col items-center gap-20 w-full">
             <div className="relative w-full h-[1140px] bg-cover bg-right">
-                <img src='/Bg.svg' className="absolute inset-0 w-full h-full object-cover object-right p-0 m-0" alt="Background Image" />
+                <img src='/Bg.svg' className="absolute inset-0 w-full h-full object-cover object-right p-0 m-0" alt="Background" />
                 <div className="absolute inset-0 flex flex-col items-center justify-center gap-10 text-center px-6 md:px-10 lg:px-20">
                     <h1 className="text-6xl md:text-8xl font-bold font-montserrat">Triangle Agency</h1>
                     <p className="text-base md:text-xl font-normal font-abel leading-[28px] md:leading-[33px] tracking-wide">
